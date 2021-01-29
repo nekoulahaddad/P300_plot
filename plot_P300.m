3b21e2a277808ee424ea70d254326cfd2926c69f
nekoula <nekoulahaddad@gmail.com>
2021-01-29 15:43:09 +0300

    add ratio plot

--- plot_P300.m
+++ plot_P300.m
@@ -1,8 +1,9 @@
-result_p300_all = xlsread('P300.xlsx');
+result_p300_all = xlsread('P300.xlsx'); 
 result_no_p300_all = xlsread('NoP300.xlsx');
 
 result_p300_all = result_p300_all';
 result_no_p300_all = result_no_p300_all';
+result_ratio_p300_no_p300 = result_p300_all./result_no_p300_all;
 
 number_of_signals_correlation = 3; 
 the_choosen_signal = [1 7 4];
@@ -10,7 +11,7 @@ frequency = [string('1HZ'),string('3HZ'),string('5HZ')];
 correlation_limit_size = 100;
 
 
-number_of_correlations = 1:correlation_limit_size^2;
+number_of_correlations = 1:25000;
 for kj=1:3
     figure
     hold on
@@ -21,4 +22,10 @@ for kj=1:3
     ylabel('maximum value of correlation')
     legend('P300','noP300')
     hold off
+    figure
+    plot(number_of_correlations,result_ratio_p300_no_p300(kj,:),'b');
+    title(frequency(kj))
+    xlabel('correlation variant')
+    ylabel('Ratio P300/NoP300')
+    legend('P300','noP300')
 end;
